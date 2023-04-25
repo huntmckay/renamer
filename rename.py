4d1b9f9f152e37ab23c4a532bd45f6d9a4fdcb90
mcstarky <huntmckay@gmail.com>
2023-04-25 14:29:48 -0400

    replaced os with pathlib, cleaned up var names

--- rename.py
+++ rename.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
 import click
-import os
+
+from pathlib import Path
 
 @click.command(help="Accepts a path as a command line argument and stores it as a variable.")
-@click.argument('path', type=click.Path(exists=True))
-def main(path):
+@click.argument('input_path', type=click.Path(exists=True))
+def main(input_path):
+    """
+    Replaces spaces in filenames with underscores in given directory.
 
+    Example usage:
+    > python rename.py /path/to/directory
+    """
+
+    path = Path(input_path)
     click.echo("replacing spaces in filenames with unscores at path: {}".format(path))
     
-    dir_list = os.listdir(path)
-    for i in dir_list:
-        j = i.split(' ')
-        if len(j) > 1:
-            x = '_'.join(j)
-            print(f"file '{i}' >>> '{x}'")
-            os.rename(f"{path}/{i}",f"{path}/{x}")
+    files_renamed = 0
+    errors = []
+    for file_path in path.iterdir():
+        if file_path.is_file():
+            new_name = file_path.name.replace(' ', '_')
+            new_path = file_path.with_name(new_name)
+            if new_path != file_path:
+                try:
+                    file_path.rename(new_path)
+                    files_renamed += 1
+                except Exception as e:
+                    errors.append(f"Error renaming file '{file_path}': {e}")
+
+    click.echo(f"{files_renamed} files renamed.")
+    for error in errors:
+        click.echo(error)
 
 if __name__ == '__main__':
     main()
